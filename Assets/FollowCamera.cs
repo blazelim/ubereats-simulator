e5b4367512cb5473d47c544d897598054a4dc3ac
Blaze Lim <blazebentleycolim@gmail.com>
2022-01-01 21:23:02 -0800

    Added script for following camera

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    [SerializeField] GameObject thingToFollow;
     // the camera's position should be the same as the car
+    void LateUpdate() 
+    {
+        transform.position = thingToFollow.transform.position + new Vector3 (0,0, -20);
+    }
 }
