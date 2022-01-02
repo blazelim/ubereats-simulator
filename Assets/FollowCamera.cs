d6ff9c26b7ebb4fc1b935356cea3adf5ae275bd2
Blaze Lim <blazebentleycolim@gmail.com>
2022-01-01 21:02:31 -0800

    added layout of level

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // the camera's position should be the same as the car
+}
