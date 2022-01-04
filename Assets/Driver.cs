a500f73654405f27d71efa4b2c97a944ef5bd60f
Blaze Lim <blazebentleycolim@gmail.com>
2022-01-03 23:08:29 -0800

    added speed up and bumpslowdown mechanics

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,6 +6,9 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+
     void Start()
     {
         
@@ -17,5 +20,18 @@ public class Driver : MonoBehaviour
         float speedAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, speedAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("enter trigger called");
+        if (other.tag == "SpeedUp") {
+            Debug.Log("Speedup called");
+            moveSpeed = boostSpeed;
+        }
     }
 }
